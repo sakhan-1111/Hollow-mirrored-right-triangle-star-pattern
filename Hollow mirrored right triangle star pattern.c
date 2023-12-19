e4d1a277d8bde33c33f779ddaab7e7b8b15af18d
Shafiqul Alam Khan <118207040+sakhan-1111@users.noreply.github.com>
2023-12-19 16:20:49 -0600

    Create Hollow mirrored right triangle star pattern.c

---
+++ Hollow mirrored right triangle star pattern.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int i, j, r;
+    printf("Enter number of rows : ");
+    scanf("%d", &r);
+ 
+    for(i=1; i<=r; i++)
+    {
+        for(j=i; j<r; j++)
+        {
+            printf(" ");
+        }
+        for(j=1; j<=i; j++)
+        {
+            if(i==r || j==1 || j==i)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    } 
+    return 0;
+} 
